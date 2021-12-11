d9b25ae99e216e9cf2eb588fcb0e663699a3e7f6
wanfengxixi <leaflove889@gmail.com>
2021-12-11 17:17:12 +0700

    Start creating the basic project

---
+++ BaseProject/BaseProject/BaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseProjectApp.swift
+//  BaseProject
+//
+//  Created by wanfengxixi on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct BaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
